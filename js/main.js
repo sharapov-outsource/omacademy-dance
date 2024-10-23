52db4f8fb6ba2c219867115fdcafc804e0e89ddc
Alexander S <alexander@sharapov.biz>
2024-10-23 19:33:43 +0600

    Make progress

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 (function ($) {
     "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-    
     
     // Initiate the wowjs
     new WOW().init();
@@ -39,25 +28,6 @@
             '<i class="bi bi-arrow-right"></i>'
         ],
     });
-
-
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
-
 
     // Dance Class carousel
     $(".class-carousel").owlCarousel({
